97439e83b542aec21ee0e3fb6e32406b85681fcc
Ayşegül Aydın <aysegul.416.aydin@gmail.com>
2024-03-13 04:06:56 +0300

    Projects are created. Entities, Configurations and Repositories added. First Migration added.

---
+++ NArchitecture.RentACar-Tobeto3A/src/webProjects/Domain/Entities/Car.cs
@@ -0,0 +1,29 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Car : BaseEntity<Guid>
+{
+    public Guid ModelId { get; set; }
+    public int ModelYear { get; set; }
+    public string Plate { get; set; }
+    public int State { get; set; }  // 1- Available 2- Rented 3-Under Maitenance
+    public double DailyPrice { get; set; }
+
+    public Model Model { get; set; }
+
+    public Car()
+    {
+    }
+
+    public Car(Guid id, Guid modelId, int modelYear,
+        string plate, int state, double dailyPrice) : this()
+    {
+        Id = id;
+        ModelId = modelId;
+        ModelYear = modelYear;
+        Plate = plate;
+        State = state;
+        DailyPrice = dailyPrice;
+    }
+}
